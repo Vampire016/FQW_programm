d2023f1677716b926b9ea76e69608846d313f498
Vampire016 <71385224+Vampire016@users.noreply.github.com>
2021-05-20 21:23:36 +0300

    Revert "Revert "Revert "beta 0.4.5"""
    This reverts commit 4addbfa524cc0fa57e238bf3e0f8ab312a952063.

--- build-QtTry4-Desktop_Qt_5_15_2_MinGW_32_bit-Debug/ui_secondwindow.h
+++ build-QtTry4-Desktop_Qt_5_15_2_MinGW_32_bit-Debug/ui_secondwindow.h
@@ -26,7 +26,6 @@ QT_BEGIN_NAMESPACE
 class Ui_SecondWindow
 {
 public:
-    QLabel *label_mLogo;
     QGroupBox *groupBox;
     QPushButton *pushButton;
     QWidget *layoutWidget;
@@ -34,10 +33,6 @@ public:
     QLabel *label;
     QLineEdit *lineEdit;
     QComboBox *comboBox;
-    QWidget *layoutWidget_2;
-    QHBoxLayout *horizontalLayout_4;
-    QLabel *label_4;
-    QLineEdit *lineEdit_2;
     QGroupBox *groupBox_2;
     QPushButton *LogIN;
     QWidget *layoutWidget1;
@@ -48,39 +43,18 @@ public:
     QHBoxLayout *horizontalLayout_3;
     QLabel *label_3;
     QLineEdit *password;
-    QPushButton *btnReturn;
 
     void setupUi(QDialog *SecondWindow)
     {
         if (SecondWindow->objectName().isEmpty())
             SecondWindow->setObjectName(QString::fromUtf8("SecondWindow"));
-        SecondWindow->resize(500, 135);
-        QSizePolicy sizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
-        sizePolicy.setHorizontalStretch(0);
-        sizePolicy.setVerticalStretch(0);
-        sizePolicy.setHeightForWidth(SecondWindow->sizePolicy().hasHeightForWidth());
-        SecondWindow->setSizePolicy(sizePolicy);
-        SecondWindow->setMinimumSize(QSize(500, 135));
-        SecondWindow->setMaximumSize(QSize(500, 135));
-        label_mLogo = new QLabel(SecondWindow);
-        label_mLogo->setObjectName(QString::fromUtf8("label_mLogo"));
-        label_mLogo->setGeometry(QRect(11, 10, 150, 115));
-        sizePolicy.setHeightForWidth(label_mLogo->sizePolicy().hasHeightForWidth());
-        label_mLogo->setSizePolicy(sizePolicy);
-        label_mLogo->setMinimumSize(QSize(150, 115));
-        label_mLogo->setSizeIncrement(QSize(150, 115));
+        SecondWindow->resize(739, 134);
         groupBox = new QGroupBox(SecondWindow);
         groupBox->setObjectName(QString::fromUtf8("groupBox"));
-        groupBox->setGeometry(QRect(169, 12, 320, 110));
-        sizePolicy.setHeightForWidth(groupBox->sizePolicy().hasHeightForWidth());
-        groupBox->setSizePolicy(sizePolicy);
-        groupBox->setMinimumSize(QSize(320, 110));
-        groupBox->setMaximumSize(QSize(320, 110));
-        groupBox->setSizeIncrement(QSize(310, 110));
+        groupBox->setGeometry(QRect(170, 10, 311, 116));
         pushButton = new QPushButton(groupBox);
         pushButton->setObjectName(QString::fromUtf8("pushButton"));
-        pushButton->setGeometry(QRect(220, 80, 75, 23));
-        pushButton->setCursor(QCursor(Qt::PointingHandCursor));
+        pushButton->setGeometry(QRect(170, 60, 75, 23));
         layoutWidget = new QWidget(groupBox);
         layoutWidget->setObjectName(QString::fromUtf8("layoutWidget"));
         layoutWidget->setGeometry(QRect(130, 20, 177, 22));
@@ -100,42 +74,12 @@ public:
         comboBox = new QComboBox(groupBox);
         comboBox->setObjectName(QString::fromUtf8("comboBox"));
         comboBox->setGeometry(QRect(10, 20, 101, 22));
-        comboBox->setCursor(QCursor(Qt::PointingHandCursor));
-        layoutWidget_2 = new QWidget(groupBox);
-        layoutWidget_2->setObjectName(QString::fromUtf8("layoutWidget_2"));
-        layoutWidget_2->setGeometry(QRect(116, 50, 191, 22));
-        horizontalLayout_4 = new QHBoxLayout(layoutWidget_2);
-        horizontalLayout_4->setObjectName(QString::fromUtf8("horizontalLayout_4"));
-        horizontalLayout_4->setContentsMargins(0, 0, 0, 0);
-        label_4 = new QLabel(layoutWidget_2);
-        label_4->setObjectName(QString::fromUtf8("label_4"));
-        QSizePolicy sizePolicy1(QSizePolicy::Fixed, QSizePolicy::Preferred);
-        sizePolicy1.setHorizontalStretch(0);
-        sizePolicy1.setVerticalStretch(0);
-        sizePolicy1.setHeightForWidth(label_4->sizePolicy().hasHeightForWidth());
-        label_4->setSizePolicy(sizePolicy1);
-        label_4->setMinimumSize(QSize(50, 0));
-        label_4->setAlignment(Qt::AlignLeading|Qt::AlignLeft|Qt::AlignVCenter);
-
-        horizontalLayout_4->addWidget(label_4);
-
-        lineEdit_2 = new QLineEdit(layoutWidget_2);
-        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
-        lineEdit_2->setEnabled(false);
-
-        horizontalLayout_4->addWidget(lineEdit_2);
-
         groupBox_2 = new QGroupBox(SecondWindow);
         groupBox_2->setObjectName(QString::fromUtf8("groupBox_2"));
-        groupBox_2->setGeometry(QRect(215, 12, 230, 110));
-        sizePolicy.setHeightForWidth(groupBox_2->sizePolicy().hasHeightForWidth());
-        groupBox_2->setSizePolicy(sizePolicy);
-        groupBox_2->setMinimumSize(QSize(230, 110));
-        groupBox_2->setSizeIncrement(QSize(230, 110));
+        groupBox_2->setGeometry(QRect(500, 10, 231, 111));
         LogIN = new QPushButton(groupBox_2);
         LogIN->setObjectName(QString::fromUtf8("LogIN"));
-        LogIN->setGeometry(QRect(120, 80, 75, 23));
-        LogIN->setCursor(QCursor(Qt::PointingHandCursor));
+        LogIN->setGeometry(QRect(70, 80, 75, 23));
         layoutWidget1 = new QWidget(groupBox_2);
         layoutWidget1->setObjectName(QString::fromUtf8("layoutWidget1"));
         layoutWidget1->setGeometry(QRect(20, 20, 189, 52));
@@ -146,11 +90,11 @@ public:
         horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
         label_2 = new QLabel(layoutWidget1);
         label_2->setObjectName(QString::fromUtf8("label_2"));
-        QSizePolicy sizePolicy2(QSizePolicy::Preferred, QSizePolicy::Preferred);
-        sizePolicy2.setHorizontalStretch(46);
-        sizePolicy2.setVerticalStretch(0);
-        sizePolicy2.setHeightForWidth(label_2->sizePolicy().hasHeightForWidth());
-        label_2->setSizePolicy(sizePolicy2);
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy.setHorizontalStretch(46);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(label_2->sizePolicy().hasHeightForWidth());
+        label_2->setSizePolicy(sizePolicy);
         label_2->setBaseSize(QSize(0, 0));
 
         horizontalLayout_2->addWidget(label_2);
@@ -167,8 +111,8 @@ public:
         horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
         label_3 = new QLabel(layoutWidget1);
         label_3->setObjectName(QString::fromUtf8("label_3"));
-        sizePolicy2.setHeightForWidth(label_3->sizePolicy().hasHeightForWidth());
-        label_3->setSizePolicy(sizePolicy2);
+        sizePolicy.setHeightForWidth(label_3->sizePolicy().hasHeightForWidth());
+        label_3->setSizePolicy(sizePolicy);
 
         horizontalLayout_3->addWidget(label_3);
 
@@ -180,9 +124,6 @@ public:
 
         verticalLayout->addLayout(horizontalLayout_3);
 
-        btnReturn = new QPushButton(groupBox_2);
-        btnReturn->setObjectName(QString::fromUtf8("btnReturn"));
-        btnReturn->setGeometry(QRect(30, 80, 75, 23));
 
         retranslateUi(SecondWindow);
 
@@ -192,16 +133,13 @@ public:
     void retranslateUi(QDialog *SecondWindow)
     {
         SecondWindow->setWindowTitle(QCoreApplication::translate("SecondWindow", "Dialog", nullptr));
-        label_mLogo->setText(QString());
         groupBox->setTitle(QCoreApplication::translate("SecondWindow", "\320\237\320\276\320\264\320\272\320\273\321\216\321\207\320\265\320\275\320\270\320\265 \320\221\320\224", nullptr));
         pushButton->setText(QCoreApplication::translate("SecondWindow", "\320\237\320\276\320\264\320\272\320\273\321\216\321\207\320\270\321\202\321\214", nullptr));
         label->setText(QCoreApplication::translate("SecondWindow", "\320\230\320\274\321\217 \320\221\320\224", nullptr));
-        label_4->setText(QCoreApplication::translate("SecondWindow", "IP - \320\260\320\264\321\200\320\265\321\201", nullptr));
         groupBox_2->setTitle(QCoreApplication::translate("SecondWindow", "\320\220\320\262\321\202\320\276\321\200\320\270\320\267\320\260\321\206\320\270\321\217", nullptr));
         LogIN->setText(QCoreApplication::translate("SecondWindow", "Log IN", nullptr));
         label_2->setText(QCoreApplication::translate("SecondWindow", "Login", nullptr));
         label_3->setText(QCoreApplication::translate("SecondWindow", "Password", nullptr));
-        btnReturn->setText(QCoreApplication::translate("SecondWindow", "\320\222\320\265\321\200\320\275\321\203\321\202\321\214\321\201\321\217", nullptr));
     } // retranslateUi
 
 };
